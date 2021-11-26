244ec03586f27999661672cb54d7a6cbe2033e7c
Vijayakumar1907 <vijaykumaar1907@gmail.com>
2021-11-27 00:49:26 +0530

    empty classes are uploaded...

---
+++ app/src/main/java/com/example/paavai_budget_plan/Register.java
@@ -0,0 +1,14 @@
+package com.example.paavai_budget_plan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
